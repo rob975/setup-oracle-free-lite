83ffee178f8ac3b9d669b292a90cae741ff10069
Roberto Benedetti <roberto.benedetti@dedalus.eu>
2025-02-25 16:45:45 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,103 @@
+Setup Oracle Database Free Lite
+###############################
+
+.. |CI| image:: https://github.com/rob975/setup-oracle-free-lite/actions/workflows/ci.yml/badge.svg
+  :target: https://github.com/rob975/setup-oracle-free-lite/actions/workflows/ci.yml
+  :alt: CI
+
+.. |Dependabot Updates| image:: https://github.com/rob975/setup-oracle-free-lite/actions/workflows/dependabot/dependabot-updates/badge.svg
+  :target: https://github.com/rob975/setup-oracle-free-lite/actions/workflows/dependabot/dependabot-updates
+  :alt: Dependabot Updates
+
+|CI| |Dependabot Updates|
+
+`GitHub Action <https://docs.github.com/actions>`_ to set up an Oracle Database using
+`Oracle Database 23ai Free Lite Container images <https://container-registry.oracle.com/ords/ocr/ba/database/free>`_.
+
+.. list-table::
+
+  * - ⚠️
+    - Only Linux `runners <https://github.com/actions/runner-images>`_ are supported.
+
+Oracle Database 23ai Free Lite Container image contains a pre-built database,
+so the startup time is very fast.
+
+Inputs
+******
+
+No input using ``step.with`` keys is required but some are defined to customize
+the database.
+
+.. list-table::
+  :widths: 15 15 70
+  :header-rows: 1
+
+  * - Name
+    - Default
+    - Description
+  * - tag
+    - latest-lite
+    - Image tag from `Oracle Container Registry <https://container-registry.oracle.com/ords/ocr/ba/database/free>`_.
+
+      Must contain hyphen delimited ``lite`` word.
+  * - container-name
+    - oracle-db
+    - Container name.
+  * - oracle-pwd
+    - *auto generated*
+    - SYS, SYSTEM and PDBADMIN password.
+  * - oracle-pdb
+    - FREEPDB1
+    - Pluggable database name.
+  * - port
+    - 1521
+    - Port for database connections.
+  * - startup-scripts
+    - *none*
+    - Absolute path to startup scripts directory.
+  * - readiness-retries
+    - 10
+    - Readiness check retries before exiting with error.
+
+      Checks are performed every 10 seconds.
+
+Usage
+*****
+
+Basic usage
+===========
+
+.. code-block:: yaml
+
+  jobs:
+    test:
+      runs-on: ubuntu-latest
+      steps:
+        - uses: rob975/setup-oracle-free-lite@v1
+
+Custom password for SYS, SYSTEM and PDBADMIN
+============================================
+
+.. code-block:: yaml
+
+  jobs:
+    test:
+      runs-on: ubuntu-latest
+      steps:
+        - uses: rob975/setup-oracle-free-lite@v1
+          with:
+            oracle-pwd: <password>
+
+Start up scripts
+================
+
+.. code-block:: yaml
+
+  jobs:
+    test:
+      runs-on: ubuntu-latest
+      steps:
+        - uses: actions/checkout@v4
+        - uses: rob975/setup-oracle-free-lite@v1
+          with:
+            startup-scripts: ${{ github.workspace }}/tests/scripts/oracle
---
+++ setup.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025 Roberto Benedetti
+set -u
+
+# utility functions
+start_group() {
+  echo "::group::$1"
+}
+
+end_group() {
+  echo "::endgroup::"
+}
+
+success() {
+  echo "✅ $1"
+}
+
+failure() {
+  echo "❌ $1"
+}
+
+VALIDATE=true
+validate() {
+  if [[ $1 -eq 0 ]]; then
+    success "$2"
+  else
+    failure "$2"
+    VALIDATE=false
+  fi
+}
+
+start_group "🔍 Validating options"
+
+# container runtime value and validation
+# podman does not work: https://github.com/oracle/docker-images/issues/2925
+CONTAINER_RUNTIME=docker
+command -v -- "${CONTAINER_RUNTIME}" >/dev/null
+ret=$?
+validate ${ret} "Container runtime: ${CONTAINER_RUNTIME}"
+
+# tag value and validation
+: "${TAG:=latest-lite}"
+[[ ${TAG} =~ -lite(-|$) ]] && ret=0 || ret=1
+validate ${ret} "Tag: ${TAG}"
+
+# container name value and validation
+: "${CONTAINER_NAME:=oracle-db}"
+[[ ${CONTAINER_NAME} =~ ^[a-zA-Z0-9][a-zA-Z0-9_.-]*$ ]] && ret=0 || ret=1
+validate ${ret} "Container name: ${CONTAINER_NAME}"
+
+# sys, system and pdbadmin password and validation
+: "${ORACLE_PWD:=}"
+[[ ${ORACLE_PWD} ]] && secret="********" || secret="<auto generated>"
+[[ ${ORACLE_PWD} =~ \" ]] && ret=1 || ret=0
+validate ${ret} "Oracle password: ${secret}"
+
+# database name value and validation
+: "${ORACLE_PDB:=FREEPDB1}"
+ORACLE_PDB=${ORACLE_PDB^^}
+[[ ${ORACLE_PDB} =~ ^[A-Z][A-Z0-9_]*$ ]] && ret=0 || ret=1
+validate ${ret} "Oracle PDB name: ${ORACLE_PDB}"
+
+# connection port and validation
+: "${PORT:=1521}"
+[[ ${PORT} =~ ^[1-9][0-9]*$ ]] && ret=0 || ret=1
+validate ${ret} "Port: ${PORT}"
+
+# startp scripts path and validation
+: "${STARTUP_SCRIPTS:=}"
+{ [[ ! ${STARTUP_SCRIPTS} ]] || { [[ ${STARTUP_SCRIPTS:0:1} = "/" ]] &&
+  [[ -d ${STARTUP_SCRIPTS} ]]; }; } && ret=0 || ret=1
+validate ${ret} "Startup scripts: ${STARTUP_SCRIPTS:-<none>}"
+
+# readiness retries value and validation
+: "${READINESS_RETRIES:=10}"
+[[ ${READINESS_RETRIES} =~ ^[1-9][0-9]*$ ]] && ret=0 || ret=1
+validate ${ret} "Readiness check retries: ${READINESS_RETRIES}"
+
+end_group
+
+# exit with error if any option is not valid
+${VALIDATE} || exit 1
+
+start_group "🐳 Running container"
+
+# initialize the array of arguments for the runtime
+CONTAINER_ARGS=(
+  run -d
+  --name "${CONTAINER_NAME}"
+  -p "${PORT}:1521"
+  -e "ORACLE_PDB=${ORACLE_PDB}"
+)
+
+# add password if given
+[[ ${ORACLE_PWD} ]] &&
+  CONTAINER_ARGS+=(-e "ORACLE_PWD=${ORACLE_PWD}")
+
+# add scripts path if given
+[[ ${STARTUP_SCRIPTS} ]] &&
+  CONTAINER_ARGS+=(-v "${STARTUP_SCRIPTS}:/opt/oracle/scripts/startup")
+
+# run the container
+"${CONTAINER_RUNTIME}" "${CONTAINER_ARGS[@]}" \
+  "container-registry.oracle.com/database/free:${TAG}"
+ret=$?
+
+end_group
+
+# exit with error if runtime failed to run container
+[[ ${ret} -eq 0 ]] || exit 1
+
+start_group "⏰ Waiting for database to be ready"
+
+# If case of success container logs contain:
+#   - the message "DATABASE IS READY TO USE!"
+#   - optionally the execution of startup scripts
+#   - the message "The following output is now a tail of the alert.log:"
+# We wait for both messages to be on the safe side
+# The value obtained by inspecting the key .State.Health.Status" cannot be
+# trusted because the value "healthy" is returned too early
+ret=1
+count=0
+lines=0
+width=${#READINESS_RETRIES}
+while [[ ${ret} -ne 0 ]] && [[ ${count} -lt ${READINESS_RETRIES} ]]; do
+  ((count++))
+  printf "  - Try #%${width}d of %d\n" "${count}" "${READINESS_RETRIES}"
+  sleep 10
+  lines=$(
+    "${CONTAINER_RUNTIME}" logs "${CONTAINER_NAME}" 2>/dev/null |
+      grep -Ec '^(DATABASE IS READY TO USE!|The following output is now a tail of the alert\.log:)$'
+  )
+  [[ ${lines} -eq 2 ]] && ret=0 || ret=1
+done
+
+end_group
+
+# report if database is ready or not
+if [[ ${ret} -eq 0 ]]; then
+  start_group "$(success "Database is ready!")"
+else
+  status=$("${CONTAINER_RUNTIME}" inspect -f "{{.State.Status}}" \
+    "${CONTAINER_NAME}" 2>/dev/null)
+  if [[ ${status} = "running" ]]; then
+    start_group "$(failure "Database was not ready on time")"
+  else
+    start_group "$(failure "Container is not running: '${status}'")"
+  fi
+fi
+"${CONTAINER_RUNTIME}" logs "${CONTAINER_NAME}" 2>/dev/null
+end_group
+
+# exit with error if database was not ready on time
+exit ${ret}
